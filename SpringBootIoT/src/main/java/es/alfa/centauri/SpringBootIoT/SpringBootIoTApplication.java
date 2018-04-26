56af24f26a01f0d3349d7418d0b5e5a0c3403c98
gatasa <gabriel.taibo@gmail.com>
2018-04-26 21:35:05 +0200

    VERSION 0

---
+++ SpringBootIoT/src/main/java/es/alfa/centauri/SpringBootIoT/SpringBootIoTApplication.java
@@ -0,0 +1,12 @@
+package es.alfa.centauri.SpringBootIoT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootIoTApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootIoTApplication.class, args);
+	}
+}
