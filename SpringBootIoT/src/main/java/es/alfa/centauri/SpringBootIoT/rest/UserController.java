a644cc80629d59480183fe8a959cacc0fc4dd394
gatasa <gabriel.taibo@gmail.com>
2018-04-28 14:34:10 +0200

    now using @getmapping

--- SpringBootIoT/src/main/java/es/alfa/centauri/SpringBootIoT/rest/UserController.java
+++ SpringBootIoT/src/main/java/es/alfa/centauri/SpringBootIoT/rest/UserController.java
@@ -3,7 +3,10 @@ package es.alfa.centauri.SpringBootIoT.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -22,21 +25,22 @@ public class UserController {
 	@Autowired
 	UserRepository userRepository;
 	
-	@RequestMapping(value = "", method = RequestMethod.GET)
+	@GetMapping
 	public List<User> getUsers(){
 		return userRepository.findAll();
 	}
-	@RequestMapping(value = "/{name}", method = RequestMethod.GET)
-	public User getUser(@PathVariable(name="name") String name){
+	@GetMapping("/{name}")
+	public User getUser(@PathVariable String name){
 		return userRepository.findByName(name);
 	}
 	
-	@RequestMapping(value = "", method = RequestMethod.POST)
+	@PostMapping
 	public User addUser(@RequestBody  User user){
 		 return userRepository.insert(user);
 	}
-	@RequestMapping(value = "/{name}", method = RequestMethod.DELETE)
-	public void addUser(@PathVariable(name="name")  String name){
+	
+	@DeleteMapping("/{name}")
+	public void addUser(@PathVariable String name){
 		  userRepository.deleteByName(name);
 	}
 
